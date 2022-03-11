e9b8bcb7f6b8b44ad30472d5e0ddf3454ae5763a
FssAy <49795742+DmitrijVC@users.noreply.github.com>
2022-03-11 21:44:25 +0100

    Add RStats plugin

---
+++ plugin/RStats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Oxide.Core.Plugins;
+using Oxide.Ext.RStats;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("RStats", "FssAy", "0.0.1")]
+
+    class RStats : RustPlugin
+    {
+        private void Init()
+        {
+            Printer.logInfo("plugin loaded");
+        }
+    }
+}
