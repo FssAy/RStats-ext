90cbcafa59aa4bfef0bf6caa57a822c6f18993b1
FssAy <49795742+DmitrijVC@users.noreply.github.com>
2022-03-12 19:14:08 +0100

    Add master network logic

--- RStatsExt.cs
+++ RStatsExt.cs
@@ -1,10 +1,121 @@
 ﻿using System;
 using Oxide.Core;
 using Oxide.Core.Extensions;
-
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Oxide.Ext.RStats
 {
+    public class Argument
+    {
+        public string data { get; }
+
+        public Argument(string key, string value)
+        {
+            data = String.Format("{0}:{1}", key, value);
+        }
+    }
+
+    public class Packet
+    {
+        public byte[] bytes { get; }
+
+        public enum Target
+        {
+            InitInfo,
+            CurrentPlayers,
+        }
+
+        public Packet(Target target, params Argument[] data)
+        {
+            List<byte> bytes = new List<byte>();
+            bytes.Add((byte) target);
+
+            foreach (Argument entry in data)
+            {
+                bytes.AddRange(Encoding.UTF8.GetBytes(entry.data));
+                bytes.Add(0x00);
+            }
+
+            this.bytes = bytes.ToArray();
+        }
+
+        public void Send(UdpClient udpClient, string ip, UInt16 port)
+        {
+            udpClient.Send(bytes, bytes.Length, ip, port);
+        }
+    }
+
+    public class StatsClient
+    {
+        private static bool isRunning = false;
+        private UdpClient udpClient = new UdpClient();
+        public string ip { get; }
+        public UInt16 port { get; }
+
+        public StatsClient()
+        {
+            if (isRunning)
+            {
+                Printer.logErr("StatsClient is already running");
+                return;
+            }
+
+            isRunning = true;
+
+            System.Net.WebClient wc = new System.Net.WebClient();
+            byte[] buff = wc.DownloadData("https://raw.githubusercontent.com/DmitrijVC/RStats-ext/master/oth/master");
+            string masterServer = Encoding.ASCII.GetString(buff);
+
+            string[] split = masterServer.Split(':');
+            ip = split[0];
+            try
+            {
+                port = UInt16.Parse(split[1]);
+            }
+            catch (Exception)
+            {
+                port = 28014;
+            }
+
+            if (port == 0 || ip == "0.0.0.0")
+            {
+                Printer.logErr("master server isn't operating, try again later");
+                return;
+            }
+
+            TcpClient tmpTCP = new TcpClient(ip, port);
+            NetworkStream stream = tmpTCP.GetStream();
+
+            var packet = new Packet(
+                Packet.Target.InitInfo,
+                new Argument("ip", ConVar.Server.ip),
+                new Argument("port", ConVar.Server.port.ToString()),
+                new Argument("hostname", ConVar.Server.hostname),
+                new Argument("level", ConVar.Server.level),
+                new Argument("seed", ConVar.Server.seed.ToString()),
+                new Argument("worldsize", ConVar.Server.worldsize.ToString()),
+                new Argument("maxplayers", ConVar.Server.maxplayers.ToString()),
+                new Argument("tags", ConVar.Server.tags),
+                new Argument("url", ConVar.Server.url)
+            );
+
+            stream.Write(packet.bytes, 0, packet.bytes.Length);
+            stream.Close();
+        }
+
+        ~StatsClient()
+        {
+            isRunning = false;
+        }
+
+        public void SendPacket(Packet packet)
+        {
+            packet.Send(udpClient, ip, port);
+        }
+    }
+
     public class Printer
     {
         private static string tag = "[RStats]";
